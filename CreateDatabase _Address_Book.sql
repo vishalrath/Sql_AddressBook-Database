d0664c825076f6374bc24c6316f903d88d3cd5c8
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-20 10:51:26 +0530

    [vishal] Create AdressBook

---
+++ CreateDatabase _Address_Book.sql
@@ -0,0 +1,7 @@
+--Uc1 Address book create database--
+create database Address_Book;
+
+-- uc create address book table--
+create table Address_Book(First_Name varchar(20), Last_Name varchar(20), Address varchar(250),City varchar(50),State varchar(100),Zip_Code int, phone bigint, Email varchar(50));
+
+select * from Address_Book;
