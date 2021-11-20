5ab9f40b490b935dc7d2b8842fe91568c8d013a1
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-20 10:59:33 +0530

    [vishal] Insert value in Table in Address book_Database

---
+++ Uc3_Insert_Contact_AddressBook.sql
@@ -0,0 +1,14 @@
+--Uc1 Address book create database--
+create database Address_Book;
+
+-- uc2 create address book table--
+create table Address_Book(First_Name varchar(20), Last_Name varchar(20), Address varchar(250),City varchar(50),State varchar(100),Zip_Code int, phone bigint, Email varchar(50));
+
+select * from Address_Book;
+
+--uc3 insert Address book contact
+insert into Address_Book values('Vishal','Rathod','pusad','Washim','Maharastra',444404,7038762577,'vishalrathod7038@gmail.com');
+insert into Address_Book values('shashank','rao','pune','pune','Maharastra',123456,1235642333,'dagv@gmail.com');
+insert into Address_Book values('rakesh','jadhao','andheri','mumbai','Maharastra',456123,4567891236,'xyz@gmail.com');
+insert into Address_Book values('amol','chavan','nagpur','nagpur','Maharastra',445566,7569874231,'abc@gmail.com');
+select * from Address_Book;
